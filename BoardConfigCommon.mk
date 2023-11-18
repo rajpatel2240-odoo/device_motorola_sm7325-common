0e077ba6fc242f27f84b9e0dca3e278059bf22b0
rajpatel2240-odoo <56307908+rajpatel2240-odoo@users.noreply.github.com>
2023-11-18 06:16:51 +0530

    evox

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -126,7 +126,7 @@ LOC_HIDL_VERSION := 4.0
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/framework_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/device_framework_matrix.xml \
-    vendor/derp/config/device_framework_matrix.xml
+    vendor/evolution/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 
